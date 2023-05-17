26a9ed4948064f80080374066cad6489fdf2357e
Nader Nolde <n.nolde@tu-berlin.de>
2023-05-17 11:53:12 +0200

    add units to the df

--- main.py
+++ main.py
@@ -40,24 +40,24 @@ if query != '':
     st.write('Kartenansicht:')
     st.map(pd.DataFrame(data=[[lat,lon]], columns=['lat','lon']))
     
-    duration = np.array([5,10,15,20,30,45,60,90,120,180,240,360,540,720,1080,1440,2880,4320,5760,7200,8640,10080])
-    returnPeriod = np.array([1,2,3,5,10,20,30,50,100])
+    duration = ['5 min','10 min','15 min','20 min','30 min','45 min','60 min','90 min','2 h','3 h','4 h','6 h','9 h','12 h','18 h','24 h','48 h','72 h','96 h','120 h','144 h','168 h']
+    returnPeriod = ['1 a','2 a','3 a','5 a','10 a','20 a','30 a','50 a','100 a']
 
     option = st.selectbox(
         'Auswahl der Datengrundlage:',
-        ('Bemessungsniederschlag in mm', 'Bemessungsspende in L/s/ha', 'Unsicherheit')
+        ('Bemessungsniederschlag in mm', 'Bemessungsspende in L/s/ha', 'Unsicherheit in %')
     )
 
     if option == 'Bemessungsniederschlag in mm': k = 0
     elif option == 'Bemessungsspende in L/s/ha': k = 9
-    elif option == 'Unsicherheit': k = 18
+    elif option == 'Unsicherheit in %': k = 18
 
     # Create the Pandas DataFrame of the selected location
     df = pd.DataFrame(data=kostraData[:,index,k:k+9], index=duration, columns=returnPeriod)
     
     # Preview the data
     st.write('Vorschau der generierten Daten:')
-    st.dataframe(df)
+    st.dataframe(df, use_container_width=True)
     
     # Create the CSV-Download button
     csv = df.to_csv(sep=';', decimal=',').encode('utf-8')
